60c90564dd65299c632b534b847cdc8dd22ffe3d
James Schumann <jschumann@gmail.com>
2018-03-28 17:25:57 +0000

    add job_dispatch

---
+++ job_dispatch/README.md
@@ -0,0 +1,2 @@
+# Job Dispatch
+Makes outbound HTTP requests based on job queue items, and updates jobs table items based on results.
---
+++ job_dispatch/index.js
@@ -0,0 +1,13 @@
+module.exports = function (context) {
+    context.log('queueMessage = ', context.bindings.queueMessage);
+    context.log('queueTrigger = ', context.bindingData.queueTrigger);
+    context.log('expirationTime = ', context.bindingData.expirationTime);
+    context.log('insertionTime = ', context.bindingData.insertionTime);
+    context.log('nextVisibleTime = ', context.bindingData.nextVisibleTime);
+    context.log('id = ', context.bindingData.id);
+    context.log('popReceipt = ', context.bindingData.popReceipt);
+    context.log('dequeueCount = ', context.bindingData.dequeueCount);
+
+    context.log(context.bindings.originalJob);
+    context.done();
+};
